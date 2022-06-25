d805a23b4a033cca793c15e3f2bc62e0ea8b7602
Parabeac-Nest <admin@parabeac.com>
2022-06-25 05:10:47 +0000

    Auto-generated commit by Parabeac Nest

---
+++ lib/views/page_1/nuveda_user.g.dart
@@ -0,0 +1,555 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+//
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+
+class NuvedaUser extends StatefulWidget {
+  const NuvedaUser({
+    Key? key,
+  }) : super(key: key);
+  @override
+  _NuvedaUser createState() => _NuvedaUser();
+}
+
+class _NuvedaUser extends State<NuvedaUser> {
+  _NuvedaUser();
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+      child: Stack(children: [
+        Positioned(
+          left: 0,
+          width: MediaQuery.of(context).size.width * 1.0,
+          top: MediaQuery.of(context).size.height * 0.011,
+          height: MediaQuery.of(context).size.height * 0.989,
+          child: Container(
+            width: MediaQuery.of(context).size.width * 1.000,
+            height: MediaQuery.of(context).size.height * 0.989,
+            decoration: BoxDecoration(
+              color: Color(0xfff6f6f6),
+            ),
+          ),
+        ),
+        Positioned(
+          left: 324.0,
+          width: 11.658,
+          top: 683.0,
+          height: 18.333,
+          child: SvgPicture.asset(
+            'assets/images/icbaselinemobilescreenshare.svg',
+            package: 'sk',
+            width: 11.658,
+            height: 18.333,
+            fit: BoxFit.none,
+          ),
+        ),
+        Positioned(
+          left: 0,
+          width: MediaQuery.of(context).size.width * 1.0,
+          top: 0,
+          height: MediaQuery.of(context).size.height * 0.126,
+          child: Container(
+            width: MediaQuery.of(context).size.width * 1.000,
+            height: MediaQuery.of(context).size.height * 0.126,
+            decoration: BoxDecoration(
+              color: Color(0xff8593f4),
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+            ),
+          ),
+        ),
+        Positioned(
+          left: MediaQuery.of(context).size.width * 0.896,
+          width: MediaQuery.of(context).size.width * 0.064,
+          top: MediaQuery.of(context).size.height * 0.074,
+          height: MediaQuery.of(context).size.height * 0.03,
+          child: Center(
+              child: Container(
+                  width: 24.0,
+                  child: Image.asset(
+                    'assets/images/ellipse63.png',
+                    package: 'sk',
+                    width: 24.000,
+                    height: MediaQuery.of(context).size.height * 0.030,
+                    fit: BoxFit.fill,
+                  ))),
+        ),
+        Positioned(
+          left: MediaQuery.of(context).size.width * 0.912,
+          width: MediaQuery.of(context).size.width * 0.033,
+          top: MediaQuery.of(context).size.height * 0.079,
+          height: MediaQuery.of(context).size.height * 0.02,
+          child: Center(
+              child: Container(
+                  width: 12.218185424804688,
+                  child: SvgPicture.asset(
+                    'assets/images/vector.svg',
+                    package: 'sk',
+                    width: 12.218,
+                    height: MediaQuery.of(context).size.height * 0.020,
+                    fit: BoxFit.fill,
+                  ))),
+        ),
+        Positioned(
+          left: 8.0,
+          width: 121.0,
+          top: MediaQuery.of(context).size.height * 0.057,
+          height: MediaQuery.of(context).size.height * 0.149,
+          child: Image.asset(
+            'assets/images/ellipse62.png',
+            package: 'sk',
+            width: 121.000,
+            height: MediaQuery.of(context).size.height * 0.149,
+            fit: BoxFit.fill,
+          ),
+        ),
+        Positioned(
+          left: 146.0,
+          width: 72.0,
+          top: MediaQuery.of(context).size.height * 0.089,
+          height: MediaQuery.of(context).size.height * 0.023,
+          child: Container(
+              width: 72.000,
+              height: MediaQuery.of(context).size.height * 0.023,
+              child: AutoSizeText(
+                'Hi Vishwa',
+                style: TextStyle(
+                  fontFamily: 'Lato',
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                  letterSpacing: 0,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.left,
+              )),
+        ),
+        Positioned(
+          left: MediaQuery.of(context).size.width * 0.04,
+          width: MediaQuery.of(context).size.width * 0.92,
+          top: 571.0,
+          height: 151.0,
+          child: Center(
+              child: Container(
+            width: 345.000,
+            height: 151.000,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+            ),
+          )),
+        ),
+        Positioned(
+          left: 101.0,
+          width: 81.0,
+          top: 594.0,
+          height: 17.0,
+          child: Container(
+              width: 81.000,
+              height: 17.000,
+              child: AutoSizeText(
+                'User Name',
+                style: TextStyle(
+                  fontFamily: 'Lato',
+                  fontSize: 14,
+                  fontWeight: FontWeight.w700,
+                  letterSpacing: 0,
+                  color: Colors.black,
+                ),
+                textAlign: TextAlign.left,
+              )),
+        ),
+        Positioned(
+          left: 101.0,
+          width: 78.0,
+          top: 634.0,
+          height: 12.0,
+          child: Container(
+              width: 78.000,
+              height: 12.000,
+              child: AutoSizeText(
+                '+91 1111111111',
+                style: TextStyle(
+                  fontFamily: 'Lato',
+                  fontSize: 10,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0,
+                  color: Color(0x80000000),
+                ),
+                textAlign: TextAlign.left,
+              )),
+        ),
+        Positioned(
+          left: 102.0,
+          width: 96.0,
+          top: 616.0,
+          height: 12.0,
+          child: Container(
+              width: 96.000,
+              height: 12.000,
+              child: AutoSizeText(
+                'username@gmail.com',
+                style: TextStyle(
+                  fontFamily: 'Lato',
+                  fontSize: 10,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0,
+                  color: Color(0x80000000),
+                ),
+                textAlign: TextAlign.left,
+              )),
+        ),
+        Positioned(
+          left: MediaQuery.of(context).size.width * 0.04,
+          width: MediaQuery.of(context).size.width * 0.92,
+          top: 668.0,
+          height: 54.0,
+          child: Center(
+              child: Container(
+            width: 345.000,
+            height: 54.000,
+            decoration: BoxDecoration(
+              color: Color(0xff8593f4),
+              boxShadow: [
+                BoxShadow(
+                  color: Color(0x40000000),
+                  spreadRadius: 4,
+                  blurRadius: 4,
+                  offset: Offset(0, 4),
+                ),
+              ],
+            ),
+          )),
+        ),
+        Positioned(
+          left: 232.0,
+          width: 84.0,
+          top: MediaQuery.of(context).size.height * 0.844,
+          height: MediaQuery.of(context).size.height * 0.02,
+          child: Container(
+              width: 84.000,
+              height: MediaQuery.of(context).size.height * 0.020,
+              child: AutoSizeText(
+                'Refer and Earn',
+                style: TextStyle(
+                  fontFamily: 'Lato',
+                  fontSize: 13,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.left,
+              )),
+        ),
+        Positioned(
+          left: 94.0,
+          width: 32.0,
+          top: MediaQuery.of(context).size.height * 0.845,
+          height: MediaQuery.of(context).size.height * 0.021,
+          child: Container(
+              width: 32.000,
+              height: MediaQuery.of(context).size.height * 0.021,
+              child: AutoSizeText(
+                ': 202',
+                style: TextStyle(
+                  fontFamily: 'Lato',
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.left,
+              )),
+        ),
+        Positioned(
+          left: 47.0,
+          width: 25.0,
+          top: MediaQuery.of(context).size.height * 0.84,
+          height: MediaQuery.of(context).size.height * 0.031,
+          child: SvgPicture.asset(
+            'assets/images/vector.svg',
+            package: 'sk',
+            width: 25.000,
+            height: MediaQuery.of(context).size.height * 0.031,
+            fit: BoxFit.fill,
+          ),
+        ),
+        Positioned(
+          left: 55.701,
+          width: 7.0,
+          top: MediaQuery.of(context).size.height * 0.85,
+          height: MediaQuery.of(context).size.height * 0.012,
+          child: SvgPicture.asset(
+            'assets/images/vector.svg',
+            package: 'sk',
+            width: 7.000,
+            height: MediaQuery.of(context).size.height * 0.012,
+            fit: BoxFit.fill,
+          ),
+        ),
+        Positioned(
+          left: 73.0,
+          width: 20.0,
+          top: 692.0,
+          height: 8.0,
+          child: Container(
+              width: 20.000,
+              height: 8.000,
+              child: AutoSizeText(
+                'Points ',
+                style: TextStyle(
+                  fontFamily: 'Lato',
+                  fontSize: 7,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.left,
+              )),
+        ),
+        Positioned(
+          left: 37.0,
+          width: 50.0,
+          top: MediaQuery.of(context).size.height * 0.732,
+          height: MediaQuery.of(context).size.height * 0.062,
+          child: Image.asset(
+            'assets/images/ellipse64.png',
+            package: 'sk',
+            width: 50.000,
+            height: MediaQuery.of(context).size.height * 0.062,
+            fit: BoxFit.fill,
+          ),
+        ),
+        Positioned(
+          left: 303.0,
+          width: 35.0,
+          top: 605.0,
+          height: 35.0,
+          child: Image.asset(
+            'assets/images/ellipse62.png',
+            package: 'sk',
+            width: 35.000,
+            height: 35.000,
+            fit: BoxFit.none,
+          ),
+        ),
+        Positioned(
+          left: 313.0,
+          width: 16.0,
+          top: 613.0,
+          height: 16.0,
+          child: Image.asset(
+            'assets/images/rectangle1958.png',
+            package: 'sk',
+            width: 16.000,
+            height: 16.000,
+            fit: BoxFit.none,
+          ),
+        ),
+        Positioned(
+          left: MediaQuery.of(context).size.width * 0.04,
+          width: MediaQuery.of(context).size.width * 0.92,
+          top: 115.0,
+          height: 446.0,
+          child: Center(
+              child: Container(
+            width: 345.000,
+            height: 446.000,
+            decoration: BoxDecoration(
+              color: Color(0x1a8593f4),
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+            ),
+          )),
+        ),
+        Positioned(
+          left: 0,
+          width: 375.0,
+          top: 752.0,
+          height: 60.0,
+          child: Container(
+              width: 375.000,
+              height: 60.000,
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0x40000000),
+                    spreadRadius: 4,
+                    blurRadius: 4,
+                    offset: Offset(0, -2),
+                  ),
+                ],
+              ),
+              child: Stack(children: [
+                Positioned(
+                  left: 0,
+                  width: 375.0,
+                  top: 0,
+                  height: 60.0,
+                  child: Container(
+                    width: 375.000,
+                    height: 60.000,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 45.0,
+                  width: 10.0,
+                  top: 12.0,
+                  height: 10.0,
+                  child: Image.asset(
+                    'assets/images/ellipse147.png',
+                    package: 'sk',
+                    width: 10.000,
+                    height: 10.000,
+                    fit: BoxFit.none,
+                  ),
+                ),
+                Positioned(
+                  left: 115.0,
+                  width: 10.0,
+                  top: 12.0,
+                  height: 10.0,
+                  child: Image.asset(
+                    'assets/images/ellipse148.png',
+                    package: 'sk',
+                    width: 10.000,
+                    height: 10.000,
+                    fit: BoxFit.none,
+                  ),
+                ),
+                Positioned(
+                  left: 185.0,
+                  width: 10.0,
+                  top: 12.0,
+                  height: 10.0,
+                  child: Image.asset(
+                    'assets/images/ellipse149.png',
+                    package: 'sk',
+                    width: 10.000,
+                    height: 10.000,
+                    fit: BoxFit.none,
+                  ),
+                ),
+                Positioned(
+                  left: 255.0,
+                  width: 10.0,
+                  top: 12.0,
+                  height: 10.0,
+                  child: Image.asset(
+                    'assets/images/ellipse150.png',
+                    package: 'sk',
+                    width: 10.000,
+                    height: 10.000,
+                    fit: BoxFit.none,
+                  ),
+                ),
+                Positioned(
+                  left: 325.0,
+                  width: 10.0,
+                  top: 12.0,
+                  height: 10.0,
+                  child: Image.asset(
+                    'assets/images/ellipse151.png',
+                    package: 'sk',
+                    width: 10.000,
+                    height: 10.000,
+                    fit: BoxFit.none,
+                  ),
+                ),
+                Positioned(
+                  left: 37.0,
+                  width: 25.0,
+                  top: 17.0,
+                  height: 25.0,
+                  child: Image.asset(
+                    'assets/images/rectangle1952.png',
+                    package: 'sk',
+                    width: 25.000,
+                    height: 25.000,
+                    fit: BoxFit.none,
+                  ),
+                ),
+                Positioned(
+                  left: 107.0,
+                  width: 25.0,
+                  top: 17.0,
+                  height: 25.0,
+                  child: Image.asset(
+                    'assets/images/rectangle1953.png',
+                    package: 'sk',
+                    width: 25.000,
+                    height: 25.000,
+                    fit: BoxFit.none,
+                  ),
+                ),
+                Positioned(
+                  left: 247.0,
+                  width: 25.0,
+                  top: 17.0,
+                  height: 25.0,
+                  child: Image.asset(
+                    'assets/images/rectangle1954.png',
+                    package: 'sk',
+                    width: 25.000,
+                    height: 25.000,
+                    fit: BoxFit.none,
+                  ),
+                ),
+                Positioned(
+                  left: 177.0,
+                  width: 25.0,
+                  top: 17.0,
+                  height: 25.0,
+                  child: Image.asset(
+                    'assets/images/rectangle1955.png',
+                    package: 'sk',
+                    width: 25.000,
+                    height: 25.000,
+                    fit: BoxFit.none,
+                  ),
+                ),
+                Positioned(
+                  left: 317.0,
+                  width: 25.0,
+                  top: 17.0,
+                  height: 25.0,
+                  child: Image.asset(
+                    'assets/images/rectangle1956.png',
+                    package: 'sk',
+                    width: 25.000,
+                    height: 25.000,
+                    fit: BoxFit.none,
+                  ),
+                ),
+                Positioned(
+                  left: 298.0,
+                  width: 62.0,
+                  top: 0,
+                  height: 6.0,
+                  child: Container(
+                    width: 62.000,
+                    height: 6.000,
+                    decoration: BoxDecoration(
+                      color: Color(0xff8593f4),
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                    ),
+                  ),
+                ),
+              ])),
+        ),
+      ]),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
---
+++ lib/widgets/page_1/unsplash:6l_q_d_f_g_o_b1iw.g.dart
@@ -0,0 +1,32 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+//
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+
+import 'package:flutter/material.dart';
+
+class Unsplash6lQDFGOB1iw extends StatefulWidget {
+  final BoxConstraints constraints;
+
+  const Unsplash6lQDFGOB1iw(
+    this.constraints, {
+    Key? key,
+  }) : super(key: key);
+  @override
+  _Unsplash6lQDFGOB1iw createState() => _Unsplash6lQDFGOB1iw();
+}
+
+class _Unsplash6lQDFGOB1iw extends State<Unsplash6lQDFGOB1iw> {
+  _Unsplash6lQDFGOB1iw();
+
+  @override
+  Widget build(BuildContext context) {
+    return;
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
